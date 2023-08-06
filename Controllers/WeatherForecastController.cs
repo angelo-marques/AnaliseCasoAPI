08cf0821b793bf863234c324e23f969bbb937401
Angelo Marques <angelomarquesdeoliveira@gmail.com>
2023-08-06 11:28:10 -0300

    Limpando comentario.

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -7,13 +7,7 @@ namespace AnaliseCasoAPI.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-       
 
-     
         private readonly ILogger<WeatherForecastController> _logger;
         private readonly IMapper _mapper;
 
@@ -23,19 +17,6 @@ namespace AnaliseCasoAPI.Controllers
             _mapper = mapper;
         }
 
-        //[HttpGet(Name = "GetWeatherForecast")]
-        //public IEnumerable<WeatherForecast> Get()
-        //{
-        //    WeatherForecast weatherForecast = new();
-        //  var retrono =  weatherForecast.SolicitacaoAPI();
-        //    return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        //    {
-        //        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-        //        TemperatureC = Random.Shared.Next(-20, 55),
-        //        Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        //    })
-        //    .ToArray();
-        //}
 
         [HttpGet]
         [Route("WeatherForecast")]
